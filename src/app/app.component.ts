9ffc8eced19d7fd013118c167ac29ccd6be87a53
JasonOCat <jason.catbagan@gmail.com>
2023-07-30 11:00:38 +0200

    Use pokemon object to interacts with the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `Welcome to {{pokemons}}!`
+  template: `Liste de pokemons`
 })
-export class AppComponent {
-  pokemons = ['pikachu', 'salamèche', 'carapuce'];
+export class AppComponent implements OnInit {
+
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[11]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  }
 }
